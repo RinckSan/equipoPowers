1ebb769c158cf9cd26b4f67092231528a1619321
209 <209@academicos.cesde.edu.co>
2022-09-23 20:37:00 -0500

    Se calcula promedio de temperatura

--- punto1.py
+++ punto1.py
@@ -1,5 +1,12 @@
 temperaturas=[]
+promedio=0
 for i in range(5):
-    temperatura=int(input(f"Digita la {i+1} temperatura: "))
+    temperatura=int(input(f"Digita la {i+1} temperatura"))
     temperaturas.append(temperatura)
-print(temperaturas)
+
+
+for temperatura in temperaturas: 
+    promedio=promedio+temperatura
+
+
+print(f'El promedio fue {promedio / len (temperaturas)}')
