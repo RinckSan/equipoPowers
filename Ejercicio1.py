59312f6b078aabd53286f023837e24f06b97a5d3
209 <209@academicos.cesde.edu.co>
2022-09-23 20:29:17 -0500

    Se termina ejercicio Area

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -8,6 +8,8 @@ largo = int(input("Ingrese el largo del Rectangulo: "))
 ancho = int(input("Ingrese el ancho del Rectangulo: "))
 
 area = calcularArea(largo,ancho)
+for i in range(area):   
+    print("*")
 print(f'El area de su Rectangulo es {area}')
 
 
